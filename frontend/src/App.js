6070d792e0aa830715ffce55d64ec7f7b0027da3
Eugene Schemelev <schemelev.eugene@gmail.com>
2017-04-22 16:04:03 +0300

    add haiku

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,30 +3,40 @@ import './App.css';
 
 import Tree from './blocks/Tree';
 import TreeHealth from './blocks/TreeHealth';
+import { HAIKU } from './const';
+
+console.log(HAIKU);
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <div className="app__row">
+        <div id="first-screen" className="app__row">
           <div className="app__left">
             <TreeHealth />
           </div>
           <div className="app__right">
             <div className="app__half-hight">
+              {HAIKU.SPRING}
+            </div>
+            <div className="app__half-hight">
+              Node info
+              <p><a href="#second-screen">go down</a></p>
+            </div>
+          </div>
+        </div>
+        <div id="second-screen" className="app__row">
+          <div className="app__left">
+            <Tree />
+          </div>
+          <div className="app__right">
+            <div className="app__half-hight">
+              <p><a href="#first-screen">go top</a></p>
               Account
             </div>
             <div className="app__half-hight">
               Node info
             </div>
-          </div>
-        </div>
-        <div className="app__row">
-          <div className="app__left">
-            <Tree />
-          </div>
-          <div className="app__right">
-            123
           </div>
         </div>
       </div>
