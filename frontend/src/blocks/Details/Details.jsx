d31473e64f4a12c051e0d142da859368983eac98
nfedoseeva <reflexbrain@gmail.com>
2017-04-23 03:07:33 +0300

    Checking possibility to add containers
    Checking possibility to add containers. Works fine.

--- frontend/src/blocks/Details/Details.jsx
+++ frontend/src/blocks/Details/Details.jsx
@@ -14,8 +14,35 @@ class Details extends Component {
 
     render() {
         return (
-            <div className="details">
-                {this.state.title}
+            <div className="container">
+                <div className="col-lg-3">
+                    <div className="row" style={{padding: '10px', margin: '5px'}}>
+Депозит: {this.state.title.node}
+<br></br>
+Объем дерева: 
+	                </div>
+                    <div className="row" style={{padding: '10px', margin: '5px'}}>
+Общая доля:
+<br></br>
+Доля в род.п.:
+<br></br>
+Day pr
+<br></br>
+week pr
+<br></br>
+month pr
+<br></br>
+<br></br>
+Объем инвестиций:
+<br></br>
+<br></br>
+Инструменты:
+
+	                </div>
+                    <div class="row">
+рекомендации и добавление ноды
+                    </div>
+                </div>
             </div>
         );
     }
